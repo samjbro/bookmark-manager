2a5811efbcf2542d9d19252b301fc07dc9efd9d3
samjbro <sjbroughton1@gmail.com>
2016-07-26 12:33:58 +0100

    Ch10: displays links in view

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Hello BookmarkManager!'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly

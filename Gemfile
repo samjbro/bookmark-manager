7882af5e5810e1c6f8e0f63a2fac18da841ee091
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-28 16:38:47 +0100

    add ability to add multiple tags per link

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ source "https://rubygems.org"
 gem "data_mapper"
 gem 'dm-postgres-adapter'
 gem 'sinatra'
-gem 'rspec'
-gem 'capybara'
 gem 'database_cleaner'
+
+group :test, :development do
+  gem 'rspec'
+  gem 'capybara'
+  gem 'rake'
+end
--- spec/features/creating_tags_spec.rb
+++ spec/features/creating_tags_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 feature "Adding tags" do
   scenario 'I can add a single tag to a new link' do
-
-
     visit '/links/new'
     fill_in('url', with: 'http://www.soundcloud.com')
     fill_in('title', with: 'Soundcloud')
@@ -12,5 +10,16 @@ feature "Adding tags" do
     click_button('Submit')
     link = Link.first
     expect(link.tags.map(&:name)).to include('Music')
+  end
+
+  scenario 'I can add add multiple tags to a new link' do
+
+    visit '/links/new'
+    fill_in('url', with: 'http://www.soundcloud.com')
+    fill_in('title', with: 'Soundcloud')
+    fill_in('tags', with: 'music cooking')
+    click_button('Submit')
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('music', 'cooking')
   end
 end

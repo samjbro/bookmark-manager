5884fd39396942311beef64aa62d26c120861ff0
liskowsky <karollebiedzinski@gmail.com>
2016-07-28 16:33:59 +0200

    Ch16 filtering tags

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -9,4 +9,19 @@ feature "Viewing links" do
     expect(page).to have_content 'http://www.google.co.uk'
 
   end
+  scenario 'filtering links by a "bubble" tag' do
+    visit 'links/new'
+    fill_in('title', :with => 'Google')
+    fill_in('url', :with => 'http://www.google.com')
+    fill_in('tags', :with => 'bubbles')
+    click_button('Submit')
+    visit 'links/new'
+    fill_in('title', :with => 'Makers')
+    fill_in('url', :with => 'http://www.makersacademy.co.uk')
+    fill_in('tags', :with => 'makers')
+    click_button('Submit')
+    visit '/tags/bubbles'
+    expect(page).not_to have_content 'http://www.makersacademy.co.uk'
+    expect(page).to have_content 'http://www.google.com'
+  end
 end
